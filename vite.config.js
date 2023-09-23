a0f37dd533b6b73dd9d7e0ea0661fe7843379992
Akenarin Komkoon <codingthailand@gmail.com>
2023-09-23 20:59:30 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-github-actions-teach/',
+  // base: '/vue-github-actions-teach/',
   // publicDir: '/vue-github-actions-teach/',
   plugins: [vue()],
   resolve: {
